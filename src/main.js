932703bc3da7bc4ab9a614f9c00c2aa4af508702
tycsbs <tutebaisong@163.com>
2017-11-24 17:08:50 +0800

    add extractPlugin and css plugin

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-let title = 'echarts utils demo';
+import './css/common.css';
 
-function getData(){
-    let promise = new Promise((resolve,reject) => {
+function getData() {
+    let promise = new Promise((resolve, reject) => {
         let key = ~~(Math.random() * 10);
         console.log(key)
-        if(key >= 5){
+        if (key >= 5) {
             let obj = {
-                msg:"ok",
-                data:key
+                msg: "ok",
+                data: key
             };
-            resolve.call(this,obj);
-        }else{
+            resolve.call(this, obj);
+        } else {
             let obj = {
-                msg:"error",
-                data:key
+                msg: "error",
+                data: key
             };
-            reject.call(this,obj)
+            reject.call(this, obj)
         }
     })
     return promise
-} 
+}
 let container = document.querySelector('#app');
 
 getData().then((data) => {
     container.innerHTML = JSON.stringify(data)
-},(err)=>{
+}, (err) => {
     container.innerHTML = JSON.stringify(err)
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
 let path = require('path');
 let htmlWebpackPlugin = require('html-webpack-plugin');
 let cleanWebpackPlugin = require('clean-webpack-plugin');
+let extractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
-    entry: path.resolve(__dirname,'./src/js/main.js'),
+    entry: path.resolve(__dirname, './src/main.js'),
     output: {
-        path: path.resolve(__dirname,'./dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'js/[name]-[chunkhash].js'
     },
-    module:{
-        loaders:[
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: __dirname + './node_modules/',
-                include: __dirname + './src/'
-            }
-        ]
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: path.resolve(__dirname + 'node_modules'),
+            include: path.resolve(__dirname + 'src')
+        }, {
+            test: /\.css$/,
+            use: extractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'css-loader!postcss-loader'
+            }),
+            exclude: path.resolve(__dirname + 'node_modules')
+        }]
     },
-    plugins:[
+    plugins: [
         new htmlWebpackPlugin({
             template: 'index.html',
-            filename:'index.html'
+            filename: 'index.html'
         }),
         new cleanWebpackPlugin(
-            ['dist/js'],
-            {
+            ['dist'], {
                 verbose: false,
                 dry: false,
                 root: __dirname
             }
-        )
+        ),
+        new extractTextPlugin('css/[name].css')
     ]
 }
